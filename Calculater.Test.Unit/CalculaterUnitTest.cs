5e91a8d67dc70e76d82e1e895bf412252d90f524
Virkman <nvirkman@gmail.com>
2018-09-11 23:03:50 +0200

    Addet setup

--- Calculater.Test.Unit/CalculaterUnitTest.cs
+++ Calculater.Test.Unit/CalculaterUnitTest.cs
@@ -10,45 +10,47 @@ namespace Calculater.Test.Unit
     [TestFixture]
     public class CalculaterUnitTest
     {
+        private Calculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+        }
+
         [Test]
         public void Add_Add2and4return6()
         {
-            var uut = new Calculater();
             Assert.That(uut.Add(2, 4), Is.EqualTo(6));
         }
 
         [Test]
         public void Subtract_Substract6and4_return2()
         {
-            var uut = new Calculater();
             Assert.That(uut.Subtract(6, 4), Is.EqualTo(2));
         }
 
         [Test]
         public void Multiply_4and3_return12()
         {
-            var uut = new Calculater();
             Assert.That(uut.Multiply(3, 4), Is.EqualTo(12));
         }
 
         [Test]
         public void Divide_10and5_return2()
         {
-            var uut = new Calculater();
             Assert.That(uut.Divide(10, 5), Is.EqualTo(2));
         }
 
         [Test]
         public void Power_3and3_return27()
         {
-            var uut = new Calculater();
             Assert.That(uut.Power(3, 3), Is.EqualTo(27));
         }
 
         [Test]
         public void Accumulator_3addto3_Acc6()
         {
-            var uut = new Calculater();
             uut.Add(3, 3);
             bool result = uut.Accumulator == 3 + 3;
             Assert.IsTrue(result);
@@ -57,7 +59,6 @@ namespace Calculater.Test.Unit
         [Test]
         public void Accumulator_clear()
         {
-            var uut = new Calculater();
             uut.Add(5, 5);
             uut.Clear();
             bool result = uut.Accumulator == 0;
