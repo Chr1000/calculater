f7380094d0ae0760d6f1876bd28f18d99c0bbe5c
Virkman <nvirkman@gmail.com>
2018-09-11 09:48:46 +0200

    Acc Test1

--- Calculater1/Calculater.cs
+++ Calculater1/Calculater.cs
@@ -15,7 +15,7 @@ namespace Calculater
         public double Add(double a, double b)
         {
             Accumulator = a + b;
-            return a + b;
+            return Accumulator = a + b;
         }
 
         public double Subtract(double a, double b)
@@ -38,6 +38,7 @@ namespace Calculater
 
         public double Power(double x, double exp)
         {
+            Accumulator = Math.Pow(x, exp);
             return Math.Pow(x, exp);
         }
 
